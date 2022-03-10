e125ec8ca4a7c3520cd00e69c6991c68b57b0581
PaulineOC <poc215@nyu.edu>
2022-03-10 18:33:51 -0500

    Moved HUD to the bottom

--- public/js/scene.js
+++ public/js/scene.js
@@ -50,6 +50,14 @@ class Scene {
     //Push the canvas to the DOM
     let domElement = document.getElementById("canvas-container");
     domElement.appendChild(this.webGLRenderer.domElement);
+
+    const child = domElement.children[0];
+    if(child){
+      child.style.postion = 'relative !important';
+      child.classList.add('renderedCanvas');
+    }
+
+
     // this.update = this.update.bind(this);
 
     // Add Controls
